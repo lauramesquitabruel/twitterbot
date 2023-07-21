84b5e27a81ef1914533fd3d3bd0d78a2ea918f85
lauramesquitabruel <137446250+lauramesquitabruel@users.noreply.github.com>
2023-07-20 21:00:14 -0300

    Add files via upload

---
+++ twitter.py
@@ -0,0 +1,19 @@
+import tweepy 
+
+api_key = ""
+api_secret = ""
+bearer_token = ""
+acess_token = ""
+acess_secret = ""
+
+client = tweepy.Client(bearer_token, api_key, api_secret, acess_token, acess_secret)
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, acess_token, acess_secret)
+api = tweepy.API(auth)
+
+client.create_tweet(text = "se funcionar eu nao me mato hoje")
+
+client.like("")
+
+client.retweet("")
+
+client.create_tweet(in_reply_to_tweet_id="", text = "")
